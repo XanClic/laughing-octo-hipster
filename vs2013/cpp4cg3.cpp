2f8d3253f8189d0c5842be85ca4aba85a79e0f2a
Hanna Reitz <xanclic@xanclic.moe>
2014-05-13 18:14:37 +0200

    KDevelop
    + CMake
    + KDevelop-Projekt
    * cpp4cg3 an nicht-MSVC angepasst

--- vs2013/cpp4cg3.cpp
+++ vs2013/cpp4cg3.cpp
@@ -1,7 +1,13 @@
+#ifdef _WIN32
 #include <tchar.h>
+#endif
 #include "GL/freeglut.h"
 
+#ifdef _WIN32
 int _tmain(int argc, _TCHAR* argv[]) {
+#else
+int main(int argc, char *argv[]) {
+#endif
 	glutInit(&argc, argv);
 	return 0;
 }
