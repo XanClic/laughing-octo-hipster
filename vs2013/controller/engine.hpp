f774dbf5e920495be7293d1c8a1de5d5662dd5fd
roidanton <accountadresse@gmx.de>
2014-05-14 15:19:14 +0200

    Weitere Kommentare und grüner Switch

--- vs2013/controller/engine.hpp
+++ vs2013/controller/engine.hpp
@@ -6,7 +6,7 @@ namespace controller
 
 class Engine {
 public:
-    virtual void init(int &argc, char **argv) = 0;
+    virtual void init(int &argc, char **argv) = 0; // = 0 bedeutet soviel wie abstract, also dass es keine Implementation gibt
     virtual void run(void) = 0;
 };
 
--- vs2013/view/demo_window.cpp
+++ vs2013/view/demo_window.cpp
@@ -4,15 +4,17 @@
 #include "GL/freeglut.h"
 #include "demo_window.hpp" //#include "view/demo_window.hpp"
 
-
 using namespace view;
-
 
 DemoWindow::DemoWindow(size_t width, size_t height, const std::string &name) :
 
 	Window(width, height, name)
 {
+#if 0
+	glClearColor(0, 1, 0, 1.f);
+#else
     glClearColor(.1f, .2f, .3f, 1.f);
+#endif
 	tp_start = std::chrono::steady_clock::now(); // Startzeit speichern.
 	//std::chrono::time_point tp_start = std::chrono::steady_clock::now();
 }
@@ -20,7 +22,9 @@ DemoWindow::DemoWindow(size_t width, size_t height, const std::string &name) :
 bool view::DemoWindow::display(void)
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
+#if 0
+	return false;
+#endif
 
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
@@ -30,11 +34,10 @@ bool view::DemoWindow::display(void)
 	std::chrono::steady_clock::time_point tp_now = std::chrono::steady_clock::now();
     //std::chrono::time_point<std::chrono::steady_clock> tp_now = std::chrono::steady_clock::now(); // VS kann damit nicht umgehen.
 
-	std::chrono::duration<float> time_diff = std::chrono::duration_cast<std::chrono::duration<double>>(tp_now - tp_start);
+	std::chrono::duration<float> time_diff = std::chrono::duration_cast<std::chrono::duration<double>>(tp_now - tp_start); // Um fancy zu sein, std::ratio<5, 360> als zweiten std::chrono::duration tpl Param nutzen.
 
     glRotatef(time_diff.count() / 5 * 360, 0.f, 0.f, 1.f);
-
-
+	
     glColor3f(.75f, .75f, .75f);
 
 #if 1
@@ -55,7 +58,7 @@ bool view::DemoWindow::display(void)
     glVertex3i( 1, -1, -1); glVertex3i( 1, -1,  1);
     glEnd();
 #else
-    glutWireCube(2.);
+	glutWireCube(2.); //glutWireTeapot(2.);
 #endif
 
 
